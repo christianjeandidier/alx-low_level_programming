7e69156cbee389b57232829ffbc6b1d793db764a
christianjeandidier <ndo.jeandidier>
2023-08-15 23:11:09 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,22 @@
  */
 int print_sign(int n)
 {
+	int negative = -1;
+	char ch = (char) negative;
+
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+      	}
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	}
+	else
 	{
 		_putchar('-');
+		return (ch);
 	}
-		return (48);
 }
