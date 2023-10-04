8e115c50922fcc6174d36cf62f189de3305d5042
christianjeandidier <ndo.jeandidier@gmail.com>
2023-10-04 07:59:26 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,11 +33,11 @@ char *create_buffer(char *file)
  */
 void close_file(int fd)
 {
-	int c;
+	int e;
 
-	c = close(fd);
+	e = close(fd);
 
-	if (c == -1)
+	if (e == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -53,7 +53,7 @@ void close_file(int fd)
  *
  * Description: If the aRgUmEnt couNt is iNcoRreCt - exit code 97.
  * If file_from does not exIst or can not be read - exit code 98.
- * If file_to cannot be cReAted or wrItTen to - exit code 99.
+ * If file_to can not be cReAted or wrItTen to - exit code 99.
  * If file_to or file_from can not be closed - exit code 100.
  */
 int main(int argc, char *argv[])
