5cb7376a45813d50beb9ddcf8715aeb50ac47bf1
christianjeandidier <ndo.jeandidier@gmail.com>
2023-10-30 17:42:04 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	e = read(fd, buf, letters);
 	r = write(STDOUT_FILENO, buf, e);
