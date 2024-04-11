a42b0e589e18972de0f55d23a73cf6a882bb4a37
christianjeandidier <ndo.jeandidier@gmail.com>
2024-04-11 14:23:22 -0700

    9. Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,38 +1,42 @@
 #include "search_algos.h"
 
 /**
-* interpolation_search -	function that searches for a value in
-* an array of integers using the interpolation search algorithm
-* @array: array of integers
-* @size: size of array
-* @value: value that i searched in array
-* Return: return the index of value, and if it doesn't exist return -1
-*/
+ * interpolation_search - searches for a value in an array of integers
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the first where value is located or -1 on failure or not found
+ */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, mid, high;
+	size_t i = 0, low = 0, high = size - 1;
 
-	low = 0;
-	high = (int) size - 1;
-
-	if (!array || size == 0)
-		return (-1);
-	while (array[low] != array[high] && value >= array[low] &&
-	       value <= array[high])
+	if (array)
 	{
-		mid = low + (((double) (high - low) / (array[high] - array[low])) *
-		             (value - array[low]));
+		while ((value >= array[low]))
+		{
+			i = low + (((double)(high - low) /
+						(array[high] - array[low])) * (value - array[low]));
 
-		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
-		if (value > array[mid])
-			low = mid + 1;
-		else if (value < array[mid])
-			high = mid - 1;
-		else
-			return (mid);
+			if (i > size)
+			{
+				printf("Value checked array[%lu] is out of range\n", i);
+				break;
+			}
+
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+			if (array[i] < value)
+				low = i + 1;
+			else if (array[i] > value)
+				high = i - 1;
+			else
+				return (i);
+		}
 	}
-	mid = low + (((double) (high - low) / (array[high] - array[low])) *
-	             (value - array[low]));
-	printf("Value checked array[%ld] is out of range\n", mid);
+
 	return (-1);
 }
